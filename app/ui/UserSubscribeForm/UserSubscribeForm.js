e4e4e4d43788cdd271ec606a46049eae4936f7c7
dimuch <dmytro.kostylov@gmail.com>
2024-10-06 09:31:53 +0200

    Test subscribe form integration

--- app/ui/UserSubscribeForm/UserSubscribeForm.js
+++ app/ui/UserSubscribeForm/UserSubscribeForm.js
@@ -4,8 +4,8 @@ import { Box, Typography, useTheme } from '@mui/material';
 import Script from 'next/script';
 import { useEffect, useRef } from 'react';
 
-const spFormId = '4c48f7572aaa6dccafeaa395fcced4fc814a4855e73df659defce7df955a4041';
-// const spFormId = 'f4c3df4d796cf4aeca27605e355fdc84dd082aaf10a69c4b884e39bcc90fcb35';
+// const spFormId = '4c48f7572aaa6dccafeaa395fcced4fc814a4855e73df659defce7df955a4041';
+const spFormId = 'f4c3df4d796cf4aeca27605e355fdc84dd082aaf10a69c4b884e39bcc90fcb35';
 let formContent;
 
 const UserSubscribeForm = () => {
@@ -45,6 +45,11 @@ const UserSubscribeForm = () => {
         ref={userSubscribeForm}
         id={'userSubscribeForm'}
       />
+
+      {/*<Script*/}
+      {/*  src="https://web.webformscr.com/apps/fc3/build/loader.js"*/}
+      {/*  sp-form-id={spFormId}*/}
+      {/*/>*/}
     </>
   );
 };
