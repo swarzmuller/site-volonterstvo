131a8f4e83e2b5cca99390ea3eab6278e8a2e58d
dimuch <dmytro.kostylov@gmail.com>
2024-10-06 18:57:46 +0200

    Added multilanguage support

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -1,14 +1,16 @@
 'use client'
 import Link from 'next/link';
 import { Box, Typography } from '@mui/material';
+import { useTranslations } from 'next-intl';
 
 const Page = () => {
+  const t = useTranslations('AboutUs');
   return (
    <Box>
      <Typography
        variant={"h2"}
      >
-       About us
+       {t('title')}
      </Typography>
      <Link
        key={"/"}
