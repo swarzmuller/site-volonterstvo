28685728601aa0233d18c6c08dae56df43338879
dimuch <dmytro.kostylov@gmail.com>
2024-10-06 20:24:32 +0200

    Added multilanguage support

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+// };
+//
+// export default nextConfig;
 
-export default nextConfig;
+
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withNextIntl(nextConfig);
--- src/context/LocaleContext/LocaleContext.js
+++ src/context/LocaleContext/LocaleContext.js
@@ -1,38 +1,38 @@
-'use client';
-// Create a context with a default value
-import { createContext, useEffect, useState } from 'react';
-import { IntlProvider } from 'next-intl';
-
-export const DEFAULT_LOCALE = 'en';
-
-const LandContext = createContext();
-
-export const LangProvider = ( {children} ) => {
-  const [locale, setLocale] = useState();
-  const [messages, setMessages] = useState();
-
-
-  const toggleLocale = async( selectedLocale ) => {
-    const localeMessages = await import(`../../../public/messages/${selectedLocale}.json`);
-    setLocale(selectedLocale);
-    setMessages(localeMessages);
-  };
-
-  useEffect(() => {
-    toggleLocale(DEFAULT_LOCALE)
-  }, []);
-
-  if(!locale || !messages) {
-    return null;
-  }
-
-  return (
-    <LandContext.Provider value={{locale, toggleLocale}}>
-      <IntlProvider messages={messages} locale={locale}>
-        {children}
-      </IntlProvider>
-    </LandContext.Provider>
-  );
-};
-
-export default LandContext;
+// 'use client';
+// // Create a context with a default value
+// import { createContext, useEffect, useState } from 'react';
+// import { IntlProvider } from 'next-intl';
+//
+// export const DEFAULT_LOCALE = 'en';
+//
+// const LandContext = createContext();
+//
+// export const LangProvider = ( {children} ) => {
+//   const [locale, setLocale] = useState();
+//   const [messages, setMessages] = useState();
+//
+//
+//   const toggleLocale = async( selectedLocale ) => {
+//     const localeMessages = await import(`../../../public/messages/${selectedLocale}.json`);
+//     setLocale(selectedLocale);
+//     setMessages(localeMessages);
+//   };
+//
+//   useEffect(() => {
+//     toggleLocale(DEFAULT_LOCALE)
+//   }, []);
+//
+//   if(!locale || !messages) {
+//     return null;
+//   }
+//
+//   return (
+//     <LandContext.Provider value={{locale, toggleLocale}}>
+//       <IntlProvider messages={messages} locale={locale}>
+//         {children}
+//       </IntlProvider>
+//     </LandContext.Provider>
+//   );
+// };
+//
+// export default LandContext;
