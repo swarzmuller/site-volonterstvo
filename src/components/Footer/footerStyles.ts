01f39267d355abaf5a77cf01a43bab0f746a0230
Vsord <stanislav060977@gmail.com>
2024-10-16 15:53:43 +0300

    few_styles_added

---
+++ src/components/Footer/footerStyles.ts
@@ -0,0 +1,23 @@
+import { css } from "@emotion/react";
+import { roboto } from "@/src/theme/fonts";
+import theme from "@/src/theme/theme";
+
+export const FooterMainBox = css`
+    height: 454.32px;
+    background-color: #2C4893;
+    color: white;
+`;
+
+export const FooterDescriptionBox = css`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const FooterBtnBox = css`
+    display: flex;
+    justify-content: space-around;
+`;
+
+export const FooterRightsQuoteBox = css`
+    text-align: center;
+`;
