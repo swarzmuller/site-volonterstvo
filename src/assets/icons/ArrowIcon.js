86fc320a2992dc1f0796f711dd5eb4324d3c7b04
rasty <hvozdetskiyyrii@gmail.com>
2024-10-17 20:16:54 +0300

    fix(Stats): made mobile version, add translate

---
+++ src/assets/icons/ArrowIcon.js
@@ -0,0 +1,8 @@
+export const ArrowIcon = () => {
+   return (
+      <svg width="35" height="34" viewBox="0 0 35 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <path d="M10.4289 16.9709H24.5711M24.5711 16.9709L18.2071 10.607M24.5711 16.9709L18.2071 23.3349"
+               stroke="#1C1C1C" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      </svg>
+   );
+};
--- src/components/PagesContent/MainIndex/components/Stats/Stats.tsx
+++ src/components/PagesContent/MainIndex/components/Stats/Stats.tsx
@@ -1,56 +1,61 @@
-import {Grid2, Stack, Typography, useTheme} from "@mui/material";
+import {Box, Grid2, Typography, useMediaQuery, useTheme} from "@mui/material";
 import {useTranslations} from "next-intl";
-import {DATA} from "@/src/components/PagesContent/MainIndex/components/Stats/Stats.constants";
+import {css} from "@emotion/react";
+
 import Splitter, {LOCATION} from "@/src/components/PagesContent/shared/Splitter/Splitter";
 import {GridSize} from "@mui/material/Grid2/Grid2";
-import {css} from "@emotion/react";
+import {mock} from "@/src/components/PagesContent/MainIndex/components/Stats/mock";
+import * as S from "./styles";
+import Link from "next/link";
+import {ArrowIcon} from "@/src/assets/icons/ArrowIcon";
 
 const Stats = () => {
    const {palette} = useTheme();
    const t = useTranslations('HomePage');
+   const isMobile = useMediaQuery('(max-width: 599px)');
+
    return (
       <>
-         <Grid2
-            container
-            sx={{
-               padding: '30px 8px',
-            }}
-            spacing={4}
-            justifyContent="space-between"
-         >
-            {
-               DATA.map((item, id) => {
-                  const itemSize: GridSize = Number.isNaN(item.size) ? "grow" : item.size;
-                  return (
-                     <Grid2
-                        key={id}
-                        size={itemSize}
-                        textAlign="center"
+         <Grid2 sx={{...S.List}}>
+            {mock.map((item, index) => {
+               const itemSize: GridSize = Number.isNaN(item.size) ? "grow" : item.size;
+               const isFirstItem = index === 0;
+               return (
+                  <Grid2
+                     spacing={itemSize}
+                     key={index}
+                     sx={{...S.Item}}
+                  >
+                     <Typography
+                        variant="h5"
+                        color={palette.primary['900']}
+                        sx={{
+                           ...S.TextItem,
+                           ...(isFirstItem && {...S.FirstTextItem}),
+                        }}
                      >
-                        <Stack
-                           direction="column"
-                        >
+                        {item.value}
+                     </Typography>
+                     <Typography
+                        variant="caption"
+                        sx={{...S.Description}}
+                     >
+                        {t(item.description)}
+                     </Typography>
+                     {item?.linkText && isMobile &&
+                        <Link href={'#'}>
                            <Typography
-                              variant="h5"
-                              fontSize="64px"
-                              color={palette.primary['900']}
-                              sx={{
-                                 marginBottom: '12px',
-                              }}
+                              sx={{...S.Link}}
                            >
-                              {item.value}
+                              {t(item.linkText)}
+                              <Box sx={S.ArrowIconWrapper}>
+                                 <ArrowIcon/>
+                              </Box>
                            </Typography>
-                           <Typography
-                              variant="caption"
-                              fontSize="22px"
-                              fontWeight="normal"
-                           >
-                              {t(item.description)}
-                           </Typography>
-                        </Stack>
-                     </Grid2>
-                  )
-               })
+                        </Link>}
+                  </Grid2>
+               )
+            })
             }
          </Grid2>
          <Splitter
