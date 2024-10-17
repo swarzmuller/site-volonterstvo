ad8082f2539f288cac263273b706ecf79f3f36dc
rasty <hvozdetskiyyrii@gmail.com>
2024-10-17 17:09:35 +0300

    fix(HeaderAchievements): made mobile version

--- src/components/PagesContent/MainIndex/components/Headers/HeaderAchievements.tsx
+++ src/components/PagesContent/MainIndex/components/Headers/HeaderAchievements.tsx
@@ -1,46 +1,56 @@
 import {Stack, Typography, useTheme} from "@mui/material";
 import {useTranslations} from "next-intl";
 import Splitter, {LOCATION} from "@/src/components/PagesContent/shared/Splitter/Splitter";
+import * as S from "./styles";
+import {css} from "@emotion/react";
 
 const HeaderAchievements = () => {
-  const {palette} = useTheme();
-  const t = useTranslations('HomePage');
-  return (
-    <>
-      <Stack
-        direction="row"
-        sx={{
-          marginTop: '180px',
-          marginBottom: '80px',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-        }}
-      >
-        <Typography
-          variant="h2"
-        >
-          {t("ourAchievements.our")}
-        </Typography>
-        &emsp;&emsp;
-        <Typography
-          variant="h2"
-          fontWeight="bold"
-        >
-          {t("ourAchievements.achievements")}
-        </Typography>
-        &emsp;&emsp;
-        <Typography
-          variant="h2"
-        >
-          {t("ourAchievements.forToday")}
-        </Typography>
-      </Stack>
-      <Splitter
-        position={LOCATION.left}
-        color={palette.divider}
-      />
-    </>
-  );
+   const {palette} = useTheme();
+   const t = useTranslations("HomePage");
+   return (
+      <>
+         <Stack
+            direction="row"
+            sx={{
+               marginTop: {
+                  xs: '100px',
+                  md: '180px',
+               },
+               marginBottom: {
+                  xs: '50px',
+                  md: '80px',
+               },
+               justifyContent: 'center',
+            }}
+         >
+            <Typography variant="h2" sx={{...S.AchievementTitle}}>
+               {t("ourAchievements.our")}{" "}
+               <Typography
+                  component="span"
+                  variant="h2"
+                  fontWeight="bold"
+                  sx={{...S.AchievementTitleSpan}}
+               >
+                  {t("ourAchievements.achievements")}
+               </Typography>{" "}
+               {t("ourAchievements.forToday")}
+            </Typography>
+         </Stack>
+         <Splitter
+            position={LOCATION.left}
+            color={palette.divider}
+            margin={'0 0 0 -15px'}
+            svgStyles={css`
+               width: 565px;
+               height: 28px;
+               @media (max-width: 600px) {
+                  width: 234px;
+                  height: 11px;
+               }
+            `}
+         />
+      </>
+   );
 };
 
 export default HeaderAchievements;
--- src/components/PagesContent/MainIndex/components/Stats/Stats.tsx
+++ src/components/PagesContent/MainIndex/components/Stats/Stats.tsx
@@ -3,61 +3,71 @@ import {useTranslations} from "next-intl";
 import {DATA} from "@/src/components/PagesContent/MainIndex/components/Stats/Stats.constants";
 import Splitter, {LOCATION} from "@/src/components/PagesContent/shared/Splitter/Splitter";
 import {GridSize} from "@mui/material/Grid2/Grid2";
+import {css} from "@emotion/react";
 
 const Stats = () => {
-  const {palette} = useTheme();
-  const t = useTranslations('HomePage');
-  return (
-    <>
-      <Grid2
-        container
-        sx={{
-          padding: '30px 8px',
-        }}
-        spacing={4}
-        justifyContent="space-between"
-      >
-        {
-          DATA.map((item, id) => {
-            const itemSize: GridSize = Number.isNaN(item.size) ? "grow" : item.size;
-            return (
-              <Grid2
-                key={id}
-                size={itemSize}
-                textAlign="center"
-              >
-                <Stack
-                  direction="column"
-                >
-                  <Typography
-                    variant="h5"
-                    fontSize="64px"
-                    color={palette.primary['900']}
-                    sx={{
-                      marginBottom: '12px',
-                    }}
-                  >
-                    {item.value}
-                  </Typography>
-                  <Typography
-                    variant="caption"
-                    fontSize="22px"
-                    fontWeight="normal"
-                  >
-                    {t(item.description)}
-                  </Typography>
-                </Stack>
-              </Grid2>
-            )
-          })
-        }
-      </Grid2>
-      <Splitter
-        position={LOCATION.right}
-        color={palette.divider}
-      />
-    </>
-  );
+   const {palette} = useTheme();
+   const t = useTranslations('HomePage');
+   return (
+      <>
+         <Grid2
+            container
+            sx={{
+               padding: '30px 8px',
+            }}
+            spacing={4}
+            justifyContent="space-between"
+         >
+            {
+               DATA.map((item, id) => {
+                  const itemSize: GridSize = Number.isNaN(item.size) ? "grow" : item.size;
+                  return (
+                     <Grid2
+                        key={id}
+                        size={itemSize}
+                        textAlign="center"
+                     >
+                        <Stack
+                           direction="column"
+                        >
+                           <Typography
+                              variant="h5"
+                              fontSize="64px"
+                              color={palette.primary['900']}
+                              sx={{
+                                 marginBottom: '12px',
+                              }}
+                           >
+                              {item.value}
+                           </Typography>
+                           <Typography
+                              variant="caption"
+                              fontSize="22px"
+                              fontWeight="normal"
+                           >
+                              {t(item.description)}
+                           </Typography>
+                        </Stack>
+                     </Grid2>
+                  )
+               })
+            }
+         </Grid2>
+         <Splitter
+            position={LOCATION.right}
+            color={palette.divider}
+            margin={'0 -15px 0 0'}
+            svgStyles={css`
+               width: 565px;
+               height: 28px;
+               @media (max-width: 600px) {
+                  width: 234px;
+                  height: 11px;
+               }
+            `}
+         />
+      </>
+   );
 };
 
 export default Stats;
