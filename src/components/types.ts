491e1a3ccef13af7914ac107588548ee03bd2a23
Vitalii Ilchenko <vitalii@Vitaliis-MacBook-Air.local>
2024-10-19 12:07:53 +0300

    Changed types

--- src/components/types.ts
+++ src/components/types.ts
@@ -27,17 +27,8 @@ export interface LayoutProps {
   children: React.ReactElement;
 }
 
-
-declare module '@mui/material/styles/createPalette' {
+declare module "@mui/material/styles/createPalette" {
   interface PaletteColor {
-    100: string;
-    200: string;
-    300: string;
-    400: string;
-    500: string;
-    600: string;
-    700: string;
-    800: string;
-    900: string;
+    [key: string]: string;
   }
 }
